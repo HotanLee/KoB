5481dedc96a9f1764d4d13ba968687da2b65f6cb
lht <1841233637@qq.com>
2023-04-30 21:59:12 +0800

    完成项目创建

--- backend/src/main/java/com/kob/backend/controller/pk/BotinfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotinfoController.java
@@ -1,6 +1,4 @@
 package com.kob.backend.controller.pk;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +7,12 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("pk/")
-public class indexController {
-    @RequestMapping("index/")
-    public String index() {
-        return "hhhhh";
+public class BotinfoController {
+    @RequestMapping("info/")
+    public Map<String, String> index() {
+        Map<String, String> bot = new HashMap<>();
+        bot.put("name", "tiger");
+        bot.put("rating", "1500");
+        return bot;
     }
 }
