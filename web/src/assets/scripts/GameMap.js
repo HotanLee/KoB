14608b11c6d31b7eb0c0456893cbb2bd4fa39c25
lht <1841233637@qq.com>
2023-05-03 22:33:36 +0800

    3.2 创建对战界面

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -1,5 +1,6 @@
 import { AcGameObject } from "./AcGameObject";
 import { Wall } from "./Wall";
+import { Snake } from "./Snake";
 
 export class GameMap extends AcGameObject {
     constructor(ctx, parent) {
@@ -14,6 +15,11 @@ export class GameMap extends AcGameObject {
 
         this.innner_walls_count = 20
         this.walls = []
+
+        this.snakes = [
+            new Snake({id: 0, color: "#4876EC", r: this.rows - 2, c: 1}, this),
+            new Snake({id: 1, color: "#F94848", r: 1, c: this.cols - 2}, this),
+        ]
     }
 
     // 检查连通性
@@ -77,11 +83,30 @@ export class GameMap extends AcGameObject {
         return true
     }
 
+    add_listening_events() {
+        this.ctx.canvas.focus()
+
+        const [snake0, snake1] = this.snakes
+        this.ctx.canvas.addEventListener("keydown", e => {
+            if (e.key === 'w') snake0.set_direction(0)
+            else if (e.key === 'd') snake0.set_direction(1)
+            else if (e.key === 's') snake0.set_direction(2)
+            else if (e.key === 'a') snake0.set_direction(3)
+            else if (e.key === 'ArrowUp') snake1.set_direction(0);
+            else if (e.key === 'ArrowRight') snake1.set_direction(1);
+            else if (e.key === 'ArrowDown') snake1.set_direction(2);
+            else if (e.key === 'ArrowLeft') snake1.set_direction(3);
+
+        })
+    }
+
     start() {
         for (let i = 0; i < 1000; i ++ ) {
             if (this.create_walls()) 
                 break 
         }
+
+        this.add_listening_events()
     }
 
     update_size() {
@@ -90,8 +115,48 @@ export class GameMap extends AcGameObject {
         this.ctx.canvas.height = this.L * this.rows;
     }
 
+    check_ready() { // 判断两蛇是否准备好进入下回合
+        for (const snake of this.snakes) {
+            if (snake.status !== "idle") return false
+            if (snake.direction === -1) return false
+        }
+
+        return true;
+    }
+
+    next_step() {
+        for (const snake of this.snakes) {
+            snake.next_step()
+        }
+    }
+
+    check_valid(cell) {  // 检测目标位置是否合法：不会撞到墙或两条蛇的身体
+        for (const wall of this.walls) {
+            if (wall.r === cell.r && wall.c === cell.c) {
+                return false
+            }
+        }
+
+        for (const snake of this.snakes) {
+            let k = snake.cells.length
+            if (!snake.check_tail_increasing()) {  // 当蛇尾不增加时，蛇尾位置不用判断
+                k -- ;
+            }
+            for (let i = 0; i < k; i ++ ) {
+                if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c) {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
     update() {
         this.update_size()
+        if (this.check_ready()) {
+            this.next_step()   
+        }
         this.render()
     }
 
--- web/src/components/GameMap.vue
+++ web/src/components/GameMap.vue
@@ -1,6 +1,6 @@
 <template>
     <div ref="parent" class="gamemap">
-        <canvas ref="canvas"></canvas>
+        <canvas ref="canvas" tabindex="0"></canvas>
     </div>
 </template>
 
@@ -23,7 +23,6 @@ export default {
         }
     }
 }
-
 </script>
 
 <style scoped>
