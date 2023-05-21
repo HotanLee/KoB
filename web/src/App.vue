d13b16a4e98bb1890505162fbe954a0508a64310
lht <1841233637@qq.com>
2023-05-21 17:32:10 +0800

    4.2 配置Mysql与注册登录模块

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,10 +7,58 @@
 import NavBar from "@/components/NavBar.vue"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap"
+import $ from 'jquery'
 
 export default{
   components: {
     NavBar
+  },
+  setup() {
+    $.ajax({
+      url: "http://127.0.0.1:3000/user/account/token/",
+      type: "post",
+      data: {
+        username: "b",
+        password: "yb",
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      }
+    })
+
+    $.ajax({
+      url: "http://127.0.0.1:3000/user/account/info/",
+      type: "get",
+      headers: {
+        Authorization: "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzOWNiMzZjNDFhZTQ0NDU4ODY1MzcyYzVmMjExODA4YiIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTY4MzQ2MTQxNywiZXhwIjoxNjg0NjcxMDE3fQ.tt9WRU-SsIw1I527emv44RKkWQqpUouexY16IOCkHBg",
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      }
+    })
+
+    $.ajax({
+      url: "http://127.0.0.1:3000/user/account/register/",
+      type: "post",
+      data: {
+        username: "d",
+        password: "yd",
+        confirmedPassword: "yd",
+      },
+      success(resp) {
+        console.log(resp);
+      },
+      error(resp) {
+        console.log(resp);
+      }
+    })
+
   }
 }
 </script>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -5,37 +5,57 @@ import RanklistIndexView from "@/views/ranklist/RanklistIndexView"
 import RecordIndexView from "@/views/record/RecordIndexView"
 import UserBotIndexView from "@/views/user/bot/UserBotIndexView"
 import NotFound from "@/views/error/NotFound"
+import UserAccountLoginView from "@/views/user/account/UserAccountLoginView"
+import UserAccountRegisterView from "@/views/user/account/UserAccountRegisterView"
 
 const routes = [
   {
     path: "/",
     redirect: "/pk/"
   },
+
   {
     path: "/pk/",
     name: "pk_index",
     component: PkIndexView
   },
+
   {
     path: "/ranklist/",
     name: "ranklist_index",
     component: RanklistIndexView
   },
+
   {
     path: "/record/",
     name: "record_index",
     component: RecordIndexView
   },
+
   {
     path: "/user/bot/",
     name: "user_bot_index",
     component: UserBotIndexView
   },
+
   {
     path: "/404/",
     name: "404",
     component: NotFound
   },
+
+  {
+    path: "/user/account/login/",
+    name: "user_account_login",
+    component: UserAccountLoginView
+  },
+
+  {
+    path: "/user/account/register/",
+    name: "user_account_register",
+    component: UserAccountRegisterView,
+  },
+
   {
     path: "/:catchAll(.*)",
     redirect: "/404/"
